af6bafa7f8e041eb02b21f7a346e9ce20d300485
akatzATarinc <akatz@arinc.com>
2021-06-10 12:49:34 -0400

    Update DataObject.py

--- DataObject.py
+++ DataObject.py
@@ -1,5 +1,5 @@
 class DataObject(object):
-    def __init__(self, mylist):
+    def __init__(self, mylist=None):
         super(DataObject, self).__init__()
         print("Invoking the __init__ method of the data object class")
         self.mylist = mylist
